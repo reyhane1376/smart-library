998835b934d47883eae4707cff5d199a616159ae
reyhane <97376622+reyhane1376@users.noreply.github.com>
2025-03-04 10:45:34 +0330

    add hasApiToken for user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
+
     protected $fillable = [
         'name',
         'email',
