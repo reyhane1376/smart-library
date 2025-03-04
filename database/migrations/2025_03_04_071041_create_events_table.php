4bfc82e62c241d9057e03f0180110098256c40b5
reyhane <97376622+reyhane1376@users.noreply.github.com>
2025-03-04 10:41:20 +0330

    add event migration

---
+++ database/migrations/2025_03_04_071041_create_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('event_type');
+            $table->string('aggregate_type');
+            $table->unsignedBigInteger('aggregate_id');
+            $table->json('event_data');
+            $table->timestamp('occurred_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
