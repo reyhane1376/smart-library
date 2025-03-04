580761465d2fc53268a4d06ef8eac396a645a033
reyhane <97376622+reyhane1376@users.noreply.github.com>
2025-03-04 09:59:57 +0330

    fix user migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_vip')->default(false);
+            $table->integer('score')->default(100);
             $table->rememberToken();
             $table->timestamps();
         });
