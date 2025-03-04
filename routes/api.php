9914044dcbf6dfd229ab30a86d1af068874b79dc
reyhane <97376622+reyhane1376@users.noreply.github.com>
2025-03-04 10:00:17 +0330

    fix route and add v1

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::group([
+    'prefix' => 'v1',
+    'as' => 'v1.',
+], function () {
+    require('v1/api.php');
+});
+
---
+++ routes/v1/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware('api')->group(function () {
+
+/* --------------------- AUTHENTICATION ----------------------------- */
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+/* --------------------- PROTECTED ----------------------------- */
+Route::middleware('auth:sanctum')->group(function () {
+    
+    /* --------------------- BOOKS ----------------------------- */
+    Route::apiResource('books', BookController::class);
+    Route::get('books/{book}/available-copies', [BookController::class, 'availableCopies']);
+    
+    // Reservations
+    Route::get('reservations', [ReservationController::class, 'index']);
+    Route::post('reservations', [ReservationController::class, 'store']);
+    Route::put('reservations/{reservation}/cancel', [ReservationController::class, 'cancel']);
+    Route::get('copies/{copy}/reservation-queue', [ReservationController::class, 'queue']);
+    
+    // Borrowings
+    Route::get('borrowings', [BorrowingController::class, 'index']);
+    Route::post('borrowings', [BorrowingController::class, 'store']);
+    Route::put('borrowings/{borrowing}/return', [BorrowingController::class, 'return']);
+    Route::put('borrowings/{borrowing}/extend', [BorrowingController::class, 'extend']);
+});
+});
